69da2ef8e53541107a19c5290a5e10f71993c791
unknown <grzesszczesny14@gmail.com>
2020-12-06 16:34:50 +0100

    Create project service for current feature.

---
+++ src/services/project.service.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { apiConstants } from "../constants";
+import { authHeader } from "../utils/api/authHeader";
+
+export const projectService = {
+  findAll,
+  saveProject,
+  updateProject,
+  deleteProject,
+  findByTitle,
+  findByName,
+};
+
+function findAll(page) {
+  return axios.get(`${apiConstants.API_ENDOINT}/api/project?page=${page}`, {});
+}
+
+function saveProject(project) {
+  const body = JSON.stringify(project);
+
+  return axios.post(`${apiConstants.API_ENDOINT}/api/project`, body, {
+    headers: authHeader(),
+  });
+}
+
+function updateProject(updateRequest) {
+  const body = JSON.stringify(updateProject);
+
+  return axios.put(`${apiConstants.API_ENDOINT}/api/project`, body, {
+    headers: authHeader(),
+  });
+}
+
+function deleteProject(id) {
+  return axios.delete(`${apiConstants.API_ENDOINT}/api/project/{${id}}`, {
+    headers: authHeader(),
+  });
+}
+
+function findByTitle(title) {
+  return axios.get(`${apiConstants.API_ENDOINT}/api/project/title/${title}`);
+}
+
+function findByName(name) {
+  return axios.get(`${apiConstants.API_ENDOINT}/api/project/name/${name}`);
+}
