a3a1e63936c311897948b893ef13e1cce7b6af1f
unknown <grzesszczesny14@gmail.com>
2020-12-06 17:16:29 +0100

    Create a actions for the feature.

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -1,106 +1,103 @@
+import { userConstants } from "../constants";
+import { userService } from "../services";
+import { alertActions } from "./alert.actions";
 
-import { userConstants } from '../constants'
-import { userService } from '../services'
-import { alertActions } from './alert.actions'
-
-import { handleResponse } from '../utils/api'
+import { handleResponse } from "../utils/api";
 
 export const userActions = {
-    getCurrentUser,
-    checkUsernameAvailability,
-    checkEmailAvailability,
+  getCurrentUser,
+  checkUsernameAvailability,
+  checkEmailAvailability,
 };
 
 function getCurrentUser(service = userService.loadUser) {
-    return (dispatch) => {
-        dispatch(request());
+  return (dispatch) => {
+    dispatch(request());
 
-        return service().then(
-            (user) => {
-                dispatch(success(user.data));
+    return service().then(
+      (user) => {
+        dispatch(success(user.data));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.toString()));
+        dispatch(alertActions.error(error.toString()));
+      }
+    );
+  };
 
-            },
-            (error) => {
-                handleResponse(error);
-                dispatch(failure(error.toString()));
-                dispatch(alertActions.error(error.toString()));
-            }
-        );
-    };
+  function request() {
+    return { type: userConstants.GET_CURRENT_USER_REQUEST };
+  }
 
-    function request() {
-        return { type: userConstants.GET_CURRENT_USER_REQUEST };
-    }
+  function success(user) {
+    return { type: userConstants.GET_CURRENT_USER_SUCCESS, user };
+  }
 
-    function success(user) {
-        return { type: userConstants.GET_CURRENT_USER_SUCCESS, user };
-    }
-
-    function failure(error) {
-        return { type: userConstants.GET_CURRENT_USER_FAILURE, error };
-    }
+  function failure(error) {
+    return { type: userConstants.GET_CURRENT_USER_FAILURE, error };
+  }
 }
 
 function checkUsernameAvailability(
-    username,
-    service = userService.checkUsernameAvailability
+  username,
+  service = userService.checkUsernameAvailability
 ) {
-    return (dispatch) => {
-        dispatch(request(username));
+  return (dispatch) => {
+    dispatch(request(username));
 
-        return service(username).then(
-            (response) => {
-                dispatch(success(response.data.available));
-            },
-            (error) => {
-                handleResponse(error);
-                dispatch(failure(error.response.data.message));
-                dispatch(alertActions.error(error.response.data.message));
-            }
-        );
-    };
+    return service(username).then(
+      (response) => {
+        dispatch(success(response.data.available));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
 
-    function request(username) {
-        return { type: userConstants.CHECK_USER_AVAIBILITY_REQUEST, username };
-    }
+  function request(username) {
+    return { type: userConstants.CHECK_USER_AVAIBILITY_REQUEST, username };
+  }
 
-    function success(response) {
-        return { type: userConstants.CHECK_USER_AVAIBILITY_SUCCESS, response };
-    }
+  function success(response) {
+    return { type: userConstants.CHECK_USER_AVAIBILITY_SUCCESS, response };
+  }
 
-    function failure(error) {
-        return { type: userConstants.CHECK_USER_AVAIBILITY_FAILURE, error };
-    }
+  function failure(error) {
+    return { type: userConstants.CHECK_USER_AVAIBILITY_FAILURE, error };
+  }
 }
 
 function checkEmailAvailability(
-    email,
-    service = userService.checkEmailAvaibility
+  email,
+  service = userService.checkEmailAvaibility
 ) {
-    return (dispatch) => {
+  return (dispatch) => {
+    dispatch(request(email));
 
-        dispatch(request(email))
+    return service(email).then(
+      (response) => {
+        dispatch(success(response.data.available));
+      },
+      (error) => {
+        handleResponse(error);
+        dispatch(failure(error.response.data.message));
+        dispatch(alertActions.error(error.response.data.message));
+      }
+    );
+  };
 
-        return service(email).then(
-            (response) => {
-                dispatch(success(response.data.available));
-            },
-            (error) => {
-                handleResponse(error);
-                dispatch(failure(error.response.data.message));
-                dispatch(alertActions.error(error.response.data.message));
-            }
-        );
-    };
+  function request(email) {
+    return { type: userConstants.CHECK_EMAIL_AVAIBILITY_REQUEST, email };
+  }
 
-    function request(email) {
-        return { type: userConstants.CHECK_EMAIL_AVAIBILITY_REQUEST, email };
-    }
-
-    function success(response) {
-        return { type: userConstants.CHECK_EMAIL_AVAIBILITY_SUCCESS, response };
-    }
-    function failure(error) {
-        return { type: userConstants.CHECK_USER_AVAIBILITY_FAILURE, error };
-    }
+  function success(response) {
+    return { type: userConstants.CHECK_EMAIL_AVAIBILITY_SUCCESS, response };
+  }
+  function failure(error) {
+    return { type: userConstants.CHECK_USER_AVAIBILITY_FAILURE, error };
+  }
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
-export * from './authentication.service'
-export * from './user.service'
-export * from './oauth2.service'
+export * from "./authentication.service";
+export * from "./user.service";
+export * from "./oauth2.service";
+export * from "./project.service";
