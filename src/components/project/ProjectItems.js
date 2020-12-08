f38f5e86a913424e9dbdac832b6026e0c0d5422f
unknown <grzesszczesny14@gmail.com>
2020-12-08 17:05:33 +0100

    Create a half-working feature.

--- src/components/project/ProjectItems.js
+++ src/components/project/ProjectItems.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { projectActions } from "../../actions";
 import {
@@ -12,17 +12,21 @@ import InfiniteScroll from "react-infinite-scroller";
 const loader = <div className="loader">Loading ...</div>;
 
 const ProjectItems = () => {
-  const page = useSelector(projectPageSelector);
+  // const page = useSelector(projectPageSelector);
   const content = useSelector(projectsSelector);
   const last = useSelector(projectLastSelector);
 
-  const [count, setCount] = useState(0);
-
   const dispatch = useDispatch();
+
+  const [page, setPage] = useState(0);
 
   const fetchAll = useCallback(() => {
     dispatch(projectActions.findAll(page));
   }, [dispatch, page]);
+
+  useEffect(() => {
+    setPage((state) => state + 1);
+  }, []);
 
   return (
     <div>
