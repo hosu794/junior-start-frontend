ea278474e529e73104b01ac3227c68b00a9f1802
unknown <grzesszczesny14@gmail.com>
2020-12-09 18:02:53 +0100

    Create a basic class component with infinity scroll.

--- src/components/project/ProjectItems.js
+++ src/components/project/ProjectItems.js
@@ -1,55 +1,117 @@
-import React, { useCallback, useEffect, useState } from "react";
+import debounce from "lodash.debounce";
+import React, {
+  Component,
+  Fragment,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { projectActions } from "../../actions";
-import {
-  projectLastSelector,
-  projectPageSelector,
-  projectsSelector,
-} from "../../selectors/project.selectors";
 
-import InfiniteScroll from "react-infinite-scroller";
+import request from "superagent";
 
-const loader = <div className="loader">Loading ...</div>;
+class ProjectItems extends Component {
+  constructor(props) {
+    super(props);
 
-const ProjectItems = () => {
-  // const page = useSelector(projectPageSelector);
-  const content = useSelector(projectsSelector);
-  const last = useSelector(projectLastSelector);
+    // Sets up our initial state
+    this.state = {
+      error: false,
+      hasMore: true,
+      isLoading: false,
+      users: [],
+      page: 0,
+    };
 
-  const dispatch = useDispatch();
+    // Binds our scroll event handler
+    window.onscroll = debounce(() => {
+      const {
+        loadUsers,
+        state: { error, isLoading, hasMore },
+      } = this;
 
-  const [page, setPage] = useState(0);
+      // Bails early if:
+      // * there's an error
+      // * it's already loading
+      // * there's nothing left to load
+      if (error || isLoading || !hasMore) return;
 
-  const fetchAll = useCallback(() => {
-    dispatch(projectActions.findAll(page));
-  }, [dispatch, page]);
+      // Checks that the page has scrolled to the bottom
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        loadUsers();
+      }
+    }, 100);
+  }
 
-  useEffect(() => {
-    setPage((state) => state + 1);
-  }, []);
+  componentWillMount() {
+    this.loadUsers();
+  }
 
-  return (
-    <div>
-      <h1>Projects</h1>
-      <InfiniteScroll
-        pageStart={0}
-        loadMore={fetchAll}
-        hasMore={!last}
-        loader={loader}
-      >
-        <div className="tracks">
-          {content
-            ? content.map((i) => (
-                <React.Fragment>
-                  <h1>{i.title}</h1>
-                  <p>{i.body}</p>
-                </React.Fragment>
-              ))
-            : null}
-        </div>
-      </InfiniteScroll>
-    </div>
-  );
-};
+  loadUsers = () => {
+    this.setState({ isLoading: true }, () => {
+      request
+        .get(
+          `https://junior-start-backend.herokuapp.com/api/project?page=${this.state.page}`
+        )
+        .then((results) => {
+          // Creates a massaged array of user data
+          console.log(results.body.content);
+          const nextUsers = results.body.content.map((user) => ({
+            title: user.title,
+            description: user.description,
+          }));
+          console.log(nextUsers);
+          // Merges the next users into our existing users
+          this.setState({
+            // Note: Depending on the API you're using, this value may
+            // be returned as part of the payload to indicate that there
+            // is no additional data to be loaded
+            hasMore: !results.body.last,
+            isLoading: false,
+            users: [...this.state.users, ...nextUsers],
+            page: this.state.page + 1,
+          });
+
+          console.log(this.state);
+        })
+        .catch((err) => {
+          this.setState({
+            error: err.message,
+            isLoading: false,
+          });
+        });
+    });
+  };
+
+  render() {
+    const { error, hasMore, isLoading, users } = this.state;
+
+    return (
+      <div>
+        <h1>Infinite Users!</h1>
+        <p>Scroll down to load more!!</p>
+        {users.map((user) => (
+          <Fragment key={user.username}>
+            <hr />
+            <div style={{ display: "flex" }}>
+              <div>
+                <h2 style={{ marginTop: 0 }}>@{user.title}</h2>
+                <p>Description: {user.description}</p>
+              </div>
+            </div>
+          </Fragment>
+        ))}
+        <hr />
+        {error && <div style={{ color: "#900" }}>{error}</div>}
+        {isLoading && <div>Loading...</div>}
+        {!hasMore && <div>You did it! You reached the end!</div>}
+      </div>
+    );
+  }
+}
 
 export default ProjectItems;
