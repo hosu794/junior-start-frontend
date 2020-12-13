4f7a9c43829415a977d62016db67168384e57f14
unknown <grzesszczesny14@gmail.com>
2020-12-13 10:12:02 +0100

    Refactor from withFormik to useFormik.

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,31 +1,11 @@
 import React from "react";
 import "../editor/formik-demo.css";
 import "../editor/rich-editor.css";
-import { withFormik } from "formik";
+import { useFormik } from "formik";
 import { createProjectValidationSchema } from "../../utils/validation";
 
 import { RichEditorExample } from "../editor/";
 import { ContentState, EditorState } from "draft-js";
-
-const formikEnhancer = withFormik({
-  mapPropsToValues: (props) => ({
-    editorState: EditorState.createWithContent(ContentState.createFromText("")),
-    name: "",
-    title: "",
-    description: "",
-    numberOfSeats: 1,
-    repository: "",
-    recruiting: false,
-  }),
-  validationSchema: createProjectValidationSchema,
-  handleSubmit: (values, { setSubmitting }) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 1000);
-  },
-  displayName: "MyForm",
-});
 
 const MyForm = ({
   values,
@@ -38,130 +18,153 @@ const MyForm = ({
   handleReset,
   setFieldValue,
   isSubmitting,
-}) => (
-  <form onSubmit={handleSubmit}>
-    <label htmlFor="name" style={{ display: "block" }}>
-      Name
-    </label>
-    <input
-      id="name"
-      placeholder="Enter your name"
-      type="text"
-      value={values.name}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {errors.name && touched.name && (
-      <div style={{ color: "red", marginTop: ".5rem" }}>{errors.name}</div>
-    )}
-    <label htmlFor="title" style={{ display: "block" }}>
-      Title
-    </label>
-    <input
-      id="title"
-      placeholder="Enter your title"
-      type="text"
-      value={values.title}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {errors.title && touched.title && (
-      <div style={{ color: "red", marginTop: ".5rem" }}>{errors.title}</div>
-    )}
+}) => {
+  const formik = useFormik({
+    initialValues: {
+      editorState: EditorState.createWithContent(
+        ContentState.createFromText("")
+      ),
+      name: "",
+      title: "",
+      description: "",
+      numberOfSeats: 1,
+      repository: "",
+      recruiting: false,
+    },
+    validationSchema: createProjectValidationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-    <label htmlFor="description" style={{ display: "block" }}>
-      Description
-    </label>
-    <input
-      id="description"
-      placeholder="Enter your description"
-      type="text"
-      value={values.description}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {errors.description && touched.description && (
-      <div style={{ color: "red", marginTop: ".5rem" }}>
-        {errors.description}
-      </div>
-    )}
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="name" style={{ display: "block" }}>
+        Name
+      </label>
+      <input
+        id="name"
+        placeholder="Enter your name"
+        type="text"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.errors.name && formik.touched.name && (
+        <div style={{ color: "red", marginTop: ".5rem" }}>
+          {formik.errors.name}
+        </div>
+      )}
+      <label htmlFor="title" style={{ display: "block" }}>
+        Title
+      </label>
+      <input
+        id="title"
+        placeholder="Enter your title"
+        type="text"
+        value={formik.values.title}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.errors.title && formik.touched.title && (
+        <div style={{ color: "red", marginTop: ".5rem" }}>
+          {formik.errors.title}
+        </div>
+      )}
 
-    <label htmlFor="" style={{ display: "block" }}>
-      Number of seats
-    </label>
-    <input
-      id="numberOfSeats"
-      placeholder="Enter your number"
-      type="text"
-      value={values.numberOfSeats}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {errors.numberOfSeats && touched.numberOfSeats && (
-      <div style={{ color: "red", marginTop: ".5rem" }}>
-        {errors.numberOfSeats}
-      </div>
-    )}
+      <label htmlFor="description" style={{ display: "block" }}>
+        Description
+      </label>
+      <input
+        id="description"
+        placeholder="Enter your description"
+        type="text"
+        value={formik.values.description}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.errors.description && formik.touched.description && (
+        <div style={{ color: "red", marginTop: ".5rem" }}>
+          {formik.errors.description}
+        </div>
+      )}
 
-    <label htmlFor="" style={{ display: "block" }}>
-      Repository
-    </label>
-    <input
-      id="repository"
-      placeholder="Enter your repository"
-      type="text"
-      value={values.repository}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {errors.repository && touched.repository && (
-      <div style={{ color: "red", marginTop: ".5rem" }}>
-        {errors.repository}
-      </div>
-    )}
+      <label htmlFor="" style={{ display: "block" }}>
+        Number of seats
+      </label>
+      <input
+        id="numberOfSeats"
+        placeholder="Enter your number"
+        type="text"
+        value={formik.values.numberOfSeats}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.errors.numberOfSeats && formik.touched.numberOfSeats && (
+        <div style={{ color: "red", marginTop: ".5rem" }}>
+          {formik.errors.numberOfSeats}
+        </div>
+      )}
 
-    <label htmlFor="" style={{ display: "block" }}>
-      Are you recruting to project?
-    </label>
-    <input
-      id="recruiting"
-      placeholder="Enter your recruiting"
-      type="checkbox"
-      value={values.recruiting}
-      onChange={handleChange}
-      onBlur={handleBlur}
-    />
-    {errors.recruiting && touched.recruiting && (
-      <div style={{ color: "red", marginTop: ".5rem" }}>
-        {errors.recruiting}
-      </div>
-    )}
+      <label htmlFor="" style={{ display: "block" }}>
+        Repository
+      </label>
+      <input
+        id="repository"
+        placeholder="Enter your repository"
+        type="text"
+        value={formik.values.repository}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.errors.repository && formik.touched.repository && (
+        <div style={{ color: "red", marginTop: ".5rem" }}>
+          {formik.errors.repository}
+        </div>
+      )}
 
-    <label
-      htmlFor="editorState"
-      style={{ display: "block", marginTop: ".5rem" }}
-    >
-      Body
-    </label>
-    <RichEditorExample
-      editorState={values.editorState}
-      onChange={setFieldValue}
-      onBlur={handleBlur}
-    />
-    <button
-      type="button"
-      className="outline"
-      onClick={handleReset}
-      disabled={!dirty || isSubmitting}
-    >
-      Reset
-    </button>
-    <button type="submit" disabled={isSubmitting}>
-      Submit
-    </button>
-  </form>
-);
+      <label htmlFor="" style={{ display: "block" }}>
+        Are you recruting to project?
+      </label>
+      <input
+        id="recruiting"
+        placeholder="Enter your recruiting"
+        type="checkbox"
+        value={formik.values.recruiting}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.errors.recruiting && formik.touched.recruiting && (
+        <div style={{ color: "red", marginTop: ".5rem" }}>
+          {formik.errors.recruiting}
+        </div>
+      )}
 
-const ProjectForm = formikEnhancer(MyForm);
+      <label
+        htmlFor="editorState"
+        style={{ display: "block", marginTop: ".5rem" }}
+      >
+        Body
+      </label>
+      <RichEditorExample
+        editorState={formik.values.editorState}
+        onChange={formik.setFieldValue}
+        onBlur={formik.handleBlur}
+      />
+      <button
+        type="button"
+        className="outline"
+        onClick={formik.handleReset}
+        disabled={!formik.dirty || formik.isSubmitting}
+      >
+        Reset
+      </button>
+      <button type="submit" disabled={formik.isSubmitting}>
+        Submit
+      </button>
+    </form>
+  );
+};
 
-export default ProjectForm;
+export default MyForm;
