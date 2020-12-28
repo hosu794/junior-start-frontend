9d834d76f6691d4470ec2d81c907dacca4a96ec9
Paul <grzesszczesny14@gmail.com>
2020-12-28 17:43:04 +0100

    Merge and fix bugs related to layout at App.js.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import MainContent from "./components/mainContent/MainContent";
 import RightMenu from "./components/rightMenu/RightMenu";
 import Footer from "./components/footer/Footer";
 import Divider from "@material-ui/core/Divider";
-import { ContentLayout } from "./styles/contentStyles";
 import { history } from "./utils";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions, alertActions } from "./actions";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LeftMenu from "./components/leftMenu/LeftMenu";
 import { ContentGrid } from "./styles/contentStyles";
 import PropTypes from "prop-types";
 import OAuth2RedirectHandler from "./components/oauth2/OAuth2RedirectHandler";
@@ -23,7 +21,7 @@ import ProjectUpdate from "./components/project/ProjectUpdate";
 import MessengerPage from "./components/mainContent/messenger/MessengerPage";
 import AccountSettingsPage from "./components/accountPage/AccountSettingsPage";
 import Grid from "@material-ui/core/Grid";
-import { Hidden } from "@material-ui/core";
+import { Box, Hidden } from "@material-ui/core";
 
 function App() {
   const loggedIn = useSelector(loggedInSelector);
@@ -42,32 +40,37 @@ function App() {
       <Layout>
         <Navbar />
         <ContentGrid container>
-          <Grid item xs>
-            <Switch>
-              <Route
-                path="/profile"
-                component={() => <div>Profile</div>}
-                exact
-              />
-              <Route exact component={MainContent} path="/" />
-              <Route exact component={MessengerPage} path="/wiadomosci" />
-              <Route
-                exact
-                component={AccountSettingsPage}
-                path="/moje-konto/ustawienia"
-              />
-              <Route path="/project/:id" component={ProjectComponent} exact />
-              <PrivateRoute
-                path="/project/update/:id"
-                component={ProjectUpdate}
-                exact
-              />
-              <PrivateRoute path="/create/project" component={ProjectCreate} />
-              <Route
-                path="/oauth2/redirect"
-                component={OAuth2RedirectHandler}
-              />
-            </Switch>
+          <Grid xs item>
+            <Box display="flex" justifyContent="center">
+              <Switch>
+                <Route
+                  path="/profile"
+                  component={() => <div>Profile</div>}
+                  exact
+                />
+                <Route exact component={MainContent} path="/" />
+                <Route exact component={MessengerPage} path="/wiadomosci" />
+                <Route
+                  exact
+                  component={AccountSettingsPage}
+                  path="/moje-konto/ustawienia"
+                />
+                <Route path="/project/:id" component={ProjectComponent} exact />
+                <PrivateRoute
+                  path="/project/update/:id"
+                  component={ProjectUpdate}
+                  exact
+                />
+                <PrivateRoute
+                  path="/create/project"
+                  component={ProjectCreate}
+                />
+                <Route
+                  path="/oauth2/redirect"
+                  component={OAuth2RedirectHandler}
+                />
+              </Switch>
+            </Box>
           </Grid>
           <Hidden only="xs">
             <Divider orientation="vertical" flexItem />
